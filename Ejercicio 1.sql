614b02cb89eb4899ef5eaa888651811543525670
darrospide <diego.arrospide219@gmail.com>
2023-05-13 18:08:10 -0500

    Update Ejercicio 1.sql

--- Ejercicio 1.sql
+++ Ejercicio 1.sql
@@ -6,3 +6,9 @@ select Id as Número, Nombre as Categoría from Categorias
 -- nombre completo, separados sus valores con coma
 
 select Nombre +', '+ Apellido as 'Nombre Completo' from Clientes
+
+-- Devolver Nombre, Costo, Precio, Costo con dscto del 30%, Ganancia si el costo tiene un dscto del 30%, ganancia con costo regular
+-- Ganancia con costo regular y un 10% de dscto
+
+select Nombre, Costo, Precio, 0.70*Costo as 'Costo -30%', Precio - Costo * 0.70 as 'Ganancia Costo -30%', 
+Precio - Costo as 'Ganancia Regular', Precio - Costo * 0.90 as 'Ganancia con costo regular y un 10% de dscto' from Productos
